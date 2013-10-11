cf125286d4040c8202b2179d5163fec80b0a9e3d
cloudaice <cloudaice@163.com>
2013-10-11 18:20:02 +0800

    添加测试文件

---
+++ restart_test.go
@@ -0,0 +1,33 @@
+package restart
+
+import (
+	"testing"
+)
+
+func TestFoo(t *testing.T) {
+	channel := make(chan string)
+	go Foo(channel)
+	var info string
+
+	for i := 0; i < 10; i++ {
+		info = <-channel
+		if info != "Foo failed" {
+			t.Error("test restart:Foo failed")
+		}
+	}
+	t.Log("test restart:Foo pass")
+}
+
+func TestBar(t *testing.T) {
+	channel := make(chan string)
+	go Bar(channel)
+	var info string
+	for i := 0; i < 10; i++ {
+		info = <-channel
+		if info != "Bar failed" {
+			t.Error("test restart:Bar failed")
+		}
+		go Bar(channel)
+	}
+	t.Log("test restart:Bar pass")
+}
